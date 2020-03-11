7173e71baa2f6887dacc0db22e4a92087464dda0
Tamás Mező <tamas.mezo@bixindex.hu>
2020-03-11 20:54:30 +0100

    adding user schema

---
+++ api/models/CreatedUser.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema(
+  {
+    userName: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    isActive: { type: Boolean, required: true },
+    storeTime: { type: String, required: true }
+  },
+  {
+    collection: "User"
+  }
+);
+
+let model = mongoose.model("User", UserSchema);
+export default model;
